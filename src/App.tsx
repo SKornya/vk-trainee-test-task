6504875051b3bb9e2b3ce38dece2ce3e5c65fc43
SKornya <kornyakovS@gmail.com>
2024-03-07 21:51:09 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react';
+// import './App.css';
+import FactsSearch from './components/FactsSearch/FactsSearch';
+import AgeSearch from './components/AgeSearch/AgeSearch';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <FactsSearch />
+      <AgeSearch />
+    </>
+  );
+}
+
+export default App;
